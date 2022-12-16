dd9074738dc6e2a8de673b9b1707da4b2be0edee
Athyd <43757337+Athyd@users.noreply.github.com>
2022-12-16 14:39:29 +0100

    clone on drag

--- ressources/js/script/draggable.js
+++ ressources/js/script/draggable.js
@@ -1,2 +1,8 @@
-$( ".draggable" ).draggable({ snap: true, revert:"invalid" });
-$(".dropzone").droppable();
+$( ".draggable" ).draggable({ snap: true, revert:"invalid", helper: 'clone'});
+$(".dropzone").droppable({
+    drop: function (event, ui) {
+        // Move draggable into droppable
+        draggable.clone().appendTo(droppable);
+        //draggable.css({top: '5px', left: '5px'});
+    }
+});
